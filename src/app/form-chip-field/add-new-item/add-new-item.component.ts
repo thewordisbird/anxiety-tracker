10e86fb2f559bc09c85b337df0d63784a231e10a
Justin Bird <justin@thewordisbird.dev>
2021-03-23 14:50:55 -0700

    add-new-item component implemented. Data flow will be processed with data service.

--- src/app/form-chip-field/add-new-item/add-new-item.component.ts
+++ src/app/form-chip-field/add-new-item/add-new-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-new-item',
@@ -6,6 +6,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./add-new-item.component.css']
 })
 export class AddNewItemComponent implements OnInit {
+  @Input() fieldLabel: string;
   @Output() newItem = new EventEmitter<string>();
   @Output() close = new EventEmitter<void>();
 
