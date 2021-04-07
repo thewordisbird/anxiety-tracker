fc6d4063fa1b18722c5ee893b05578619f0ca3e6
Justin Bird <justin@thewordisbird.dev>
2021-04-07 08:23:21 -0700

    Refactor: removed unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { AnxietyFormComponent } from './anxiety-form/anxiety-form.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
-import { AnxietyTableComponent } from './anxiety-table/anxiety-table.component';
-import { HeaderComponent } from './header/header.component';
-import { AuthComponent } from './auth/auth.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { AuthRequired, NotAuthenticated } from './auth/auth.gaurd';
-import { SvgIconComponent } from './svg-icon/svg-icon.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 import { environment } from '../environments/environment';
-import { SymptomItemsComponent } from './symptom-items/symptom-items.component';
-import { SymptomItemComponent } from './symptom-items/symptom-item/symptom-item.component';
+
+import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { AuthRequired, NotAuthenticated } from './auth/auth.gaurd';
+import { AnxietyFormComponent } from './anxiety-form/anxiety-form.component';
+import { FormSentimentFieldComponent } from './anxiety-form/form-sentiment-field/form-sentiment-field.component';
 import { FormChipFieldComponent } from './anxiety-form/form-chip-field/form-chip-field.component';
 import { AddNewItemComponent } from './anxiety-form/form-chip-field/add-new-item/add-new-item.component';
-import { FormSentimentFieldComponent } from './anxiety-form/form-sentiment-field/form-sentiment-field.component';
+import { AnxietyTableComponent } from './anxiety-table/anxiety-table.component';
 import { TableChipDisplayComponent } from './anxiety-table/table-chip-display/table-chip-display.component';
 import { TableParagraphDisplayComponent } from './anxiety-table/table-paragraph-display/table-paragraph-display.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { LayoutComponent } from './layout/layout.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import { LogoutComponent } from './auth/logout/logout.component'
+import { LogoutComponent } from './auth/logout/logout.component';
+import { ProfileComponent } from './profile/profile.component'
 
 const appRoutes: Routes = [
   {
@@ -49,41 +46,34 @@ const appRoutes: Routes = [
     canActivate: [AuthRequired]
   },
   {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
     path: 'logout',
     component: LogoutComponent
   },
-  // {
-  //   path: 'test',
-  //   component: LogoutComponent
-  // },
   {
     path: '**',
     redirectTo: 'auth',
     pathMatch: 'full'
   }
-
 ]
-
 
 @NgModule({
   declarations: [
     AppComponent,
     AnxietyFormComponent,
     AnxietyTableComponent,
-    HeaderComponent,
     AuthComponent,
-    SvgIconComponent,
-    SymptomItemsComponent,
-    SymptomItemComponent,
     FormChipFieldComponent,
     AddNewItemComponent,
     FormSentimentFieldComponent,
     TableChipDisplayComponent,
     TableParagraphDisplayComponent,
-    NavigationComponent,
-    LayoutComponent,
     ToolbarComponent,
-    LogoutComponent
+    LogoutComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
