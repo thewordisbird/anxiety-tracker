ab941e1adf2d216464b4586b39a56d76a296014a
Justin Bird <justin@thewordisbird.dev>
2021-04-19 14:05:29 -0700

    Added firebase keys to prod enviornment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBUVS14SqKAXUtpbC0Hojb5kjjlzsKV0Ms",
+    authDomain: "anxiety-tracker-c83eb.firebaseapp.com",
+    projectId: "anxiety-tracker-c83eb",
+    storageBucket: "anxiety-tracker-c83eb.appspot.com",
+    messagingSenderId: "930678131561",
+    appId: "1:930678131561:web:509de1ef796a5a2607cbf8",
+    measurementId: "G-K9MYGP74MJ"
+  }
 };
